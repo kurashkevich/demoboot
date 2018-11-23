08b19a5f31fdda27516ed7f7e2dd56ef85361d0d
Vladislav_K <v.kurashkevich@gmail.com>
2018-11-23 16:10:51 +0300

    how to use locales

--- Thymeleaf/src/main/java/com/course/thymeleaf/ThymeleafApplication.java
+++ Thymeleaf/src/main/java/com/course/thymeleaf/ThymeleafApplication.java
@@ -2,6 +2,9 @@ package com.course.thymeleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class ThymeleafApplication {
