1b64400ac3e247d1a283acc4a4392af51745a41a
Vladislav_K <v.kurashkevich@gmail.com>
2018-11-23 09:37:09 +0300

    added shopper REST module

---
+++ mrShopperREST/src/main/java/com/mrshopper/rest/MrShopperRestApplication.java
@@ -0,0 +1,12 @@
+package com.mrshopper.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrShopperRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrShopperRestApplication.class, args);
+	}
+}
