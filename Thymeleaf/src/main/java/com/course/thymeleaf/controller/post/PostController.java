feaa00c7ac900f5584f7e347b4d40152dd88ba7e
vlad <v.kurashkevich@gmail.com>
2018-11-24 20:37:00 +0300

    handle exception example

--- Thymeleaf/src/main/java/com/course/thymeleaf/controller/post/PostController.java
+++ Thymeleaf/src/main/java/com/course/thymeleaf/controller/post/PostController.java
@@ -1,10 +1,16 @@
 package com.course.thymeleaf.controller.post;
 
+import com.course.thymeleaf.domain.Post;
 import com.course.thymeleaf.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/posts/")
@@ -22,5 +28,22 @@ public class PostController {
         model.addAttribute("posts", postService.getPosts());
         System.out.println(postService.getPosts());
         return "views/list";
+    }
+
+    @RequestMapping("/get/{slug}")
+    public String getPost(@PathVariable(value = "slug") String slug) throws Exception {
+        System.out.println(slug);
+        Post post = null;
+        if(post == null) {
+            throw new Exception(("sorry, we couldn't find any post with name: " + slug));
+        }
+        return "welcome";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String myExceptionHandler(HttpServletRequest httpServletRequest, Exception exc, Model model){
+        System.out.println("halo");
+        model.addAttribute("errorMsg", exc.getMessage());
+        return "views/error/error";
     }
 }
