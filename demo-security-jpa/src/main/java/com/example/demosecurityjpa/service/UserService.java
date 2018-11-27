479223cfc2bc43ba0290a0d91eb3ba496664484c
Vladislav_K <v.kurashkevich@gmail.com>
2018-11-27 10:18:13 +0300

    code like in the lesson

--- demo-security-jpa/src/main/java/com/example/demosecurityjpa/service/UserService.java
+++ demo-security-jpa/src/main/java/com/example/demosecurityjpa/service/UserService.java
@@ -5,18 +5,7 @@ import com.example.demosecurityjpa.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
+public interface UserService {
 
-    private UserRepository userRepository;
-
-    @Autowired
-    public void setUserRepository(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-
-    public User findByEmail(String email){
-        return userRepository.findByEmail(email);
-    }
+    public User findByEmail(String email);
 }
---
+++ demo-security-jpa/src/main/java/com/example/demosecurityjpa/service/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.demosecurityjpa.service;
+
+import com.example.demosecurityjpa.domain.User;
+import com.example.demosecurityjpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService, UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = findByEmail(username);
+        if( user == null ){
+            throw new UsernameNotFoundException(username);
+        }
+        return new UserDetailsImpl(user);
+    }
+}
