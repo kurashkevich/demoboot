58a31e125aa3c339a004817aa7375f2515255a39
Vladislav_K <v.kurashkevich@gmail.com>
2018-11-27 08:42:22 +0300

    added configuration for spring-security-jpa module

--- demo-security-jpa/src/main/java/com/example/demosecurityjpa/DemoSecurityJpaApplication.java
+++ demo-security-jpa/src/main/java/com/example/demosecurityjpa/DemoSecurityJpaApplication.java
@@ -1,12 +1,12 @@
-package com.mrshopper.rest;
+package com.example.demosecurityjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MrShopperRestApplication {
+public class DemoSecurityJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MrShopperRestApplication.class, args);
+		SpringApplication.run(DemoSecurityJpaApplication.class, args);
 	}
 }
