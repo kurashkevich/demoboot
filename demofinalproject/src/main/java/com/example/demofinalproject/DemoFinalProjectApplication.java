7b25d97be74f32da9d72f60ecb51b815ea5de2a4
Vladislav_K <v.kurashkevich@gmail.com>
2018-12-05 19:09:06 +0300

    Create module for final project

---
+++ demofinalproject/src/main/java/com/example/demofinalproject/DemoFinalProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.demofinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFinalProjectApplication.class, args);
+	}
+}
